b8d0bd62e47d5767012a31453c08cbcbfa3d3d2f
Samuel Gillispie II <spgillispie2@gmail.com>
2022-11-05 02:11:21 -0400

    Revise the URL shortener to support the latest server API with auth

--- spgill/utils/shorten/__init__.py
+++ spgill/utils/shorten/__init__.py
@@ -1,8 +1,19 @@
+# stdlib imports
+import base64
+
+# vendor imports
 import requests
 
 
-def shortenURL(url, key=None):
-    """Take a URL and return the shortened one."""
-    return requests.post(
-        "https://short.spgill.me/api", data={"url": url, "key": key}
+def shortenUrl(
+    url: str, username: str, password: str, scheme: bool = True
+) -> str:
+    """Using my personal `short.spgill.me` URL shortening service, return a shortened version of a URL."""
+    # Combine the credentials for the authorization header
+    authHeaderValue = base64.b64encode(f"{username}:{password}".encode("utf8"))
+
+    return requests.put(
+        "https://short.spgill.me/api",
+        json={"url": url, "format": "normal" if scheme else "noscheme"},
+        headers={"Proxy-Authorization": authHeaderValue},
     ).text
